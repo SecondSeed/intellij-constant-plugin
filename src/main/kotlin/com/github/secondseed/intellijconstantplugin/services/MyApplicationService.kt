b75ae0679ae8c9edd8873e5d9c85a9533c250406
GitHub Action <action@github.com>
2020-09-05 10:04:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/secondseed/intellijconstantplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.secondseed.intellijconstantplugin.services
+
+import com.github.secondseed.intellijconstantplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/secondseed/intellijconstantplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/secondseed/intellijconstantplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.secondseed.intellijconstantplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.secondseed.intellijconstantplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
